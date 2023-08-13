12e293e2424dd8a04e6c93599b833268b961a34e
Rustam <Isaev.Rustam.A@gmail.com>
2023-08-13 14:31:37 +0300

    fix import vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import path from 'path';
+import * as path from 'path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
